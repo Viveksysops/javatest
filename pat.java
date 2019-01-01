31154627d3836b6c85729b2bbc2730a46fa088f1
Viveksysops <46191681+Viveksysops@users.noreply.github.com>
2019-01-01 12:56:37 +0530

    Update pat.java

--- pat.java
+++ pat.java
@@ -2,6 +2,6 @@ class pat
 {
 
    public static void main(String[] args) {
-       System.out.println(" ## Hello Genius, Vivek ##9th push succeed !!!!! ");
+       System.out.println(" ## Hello Genius, Vivek ##7h push succeed !!!!! ");
    }
 }
